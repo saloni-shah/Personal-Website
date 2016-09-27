3f7b69fa4958ba012ae2fdb74626effe0938dbdf
Saloni Shah <saloni.shah281@gmail.com>
2016-09-26 22:54:48 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Saloni Shah</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script type="text/javascript">
+      
+var jump=function(e)
+{
+   if (e){
+       e.preventDefault();
+       var target = $(this).attr("href");
+   }else{
+       var target = location.hash;
+   }
+
+   $('html,body').animate(
+   {
+       $('html,body').scrollTo( $(target), 800 );
+   },2000,function()
+   {
+       location.hash = target;
+   });
+
+}
+
+$('html, body').hide();
+
+$(document).ready(function()
+{
+    $('a[href^=#]').bind("click", jump);
+
+    if (location.hash){
+        setTimeout(function(){
+            $('html,body').scrollTo( $(target), 800 );
+            $('html, body').scrollTop(0).show();
+            jump();
+        }, 0);
+    }else{
+        $('html, body').show();
+    }
+});
+        </script>
+        <style>
+            .main{
+                position: relative;
+            }
+         .container-fluid {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: orangered;
+            
+        }
+
+        li {
+            float: center;
+        }
+
+        li a {
+            
+            color: white !important;
+            text-align: center;
+            font-size: 18px;
+            margin-right: 15px !important;
+           
+            
+        }
+        .navbar-inverse .navbar-brand{
+            color: white !important;
+            font-size: 25px !important;
+            margin-left: 15px !important;
+        }
+        .navbar-contact{
+            color: white; 
+            font-size: 15px;
+            margin-left: 395px;
+            display:inline-block;
+            text-align: center;
+        }
+        .navbar-connect{
+            color: white; 
+            font-size: 15px;
+            margin-right: 395px;
+            float: right;
+            display:inline-block;
+            text-align: center;
+        }
+
+        li a:hover:not(.active) {
+            background-color: grey !important;
+        }
+        li a:active {
+            background-color: grey !important;
+        }
+
+      
+        .nav navbar-nav{
+            width: 100% !important;
+        }
+        h2{
+            font-size: 44px;
+            
+            
+        }
+        .row{
+            
+            width: 50% !important;
+            float: left;
+            
+        }
+        .cvbuttons{
+             text-align: center;
+        }
+        .row p{
+             text-align: justify;
+        }
+        img{
+            float: right;
+           
+            width: 530px !important;
+            height: 440px !important;
+        }
+        </style>
+    </head>
+    <body>
+        <?php
+
+        ?>
+        <div class ="main">
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                  <a class="navbar-brand" href="#"> Saloni Shah</a>
+                </div>
+        
+                <ul class="nav navbar-nav navbar-right">
+                  <li><a href="#">HOME</a></li>
+                  <li><a href="about.html#section2" class="smoothScroll">ABOUT</a></li>
+                  <li><a href="skills.php">SKILLS</a></li>
+                  <li><a href="#">EXPERIENCE</a></li>
+                  <li><a href="#">EDUCATION</a></li>
+                  <li><a href="#">CONTACT</a></li>
+                </ul>
+            </div>
+         </nav>
+      <div class="sections">
+        <section id="section1" class="no-padding-bottom active" style='padding-top:5px;'>
+            <div class="container">
+                <div class="row">
+                  <h2>Script Development Engineer Intern in Calix,Inc</h2>
+                  <p>Hello, my name is Saloni Shah. I am a Software Engineer with 2.5 years of web development experience.</p>
+                  <br>
+                  <p>I am currently an intern at Calix,Inc located in Petaluma, CA. I am actively involved in developing automation scripts for large systems
+                     using PHP, Python and JavaScript. I also participate in Scrum meetings, new project initiatives and release planning meetings. I also have 
+                     done web development using PHP and CMS like Joomla.
+                  </p>
+                  <p>
+                      I am currently pursuing my Masters in Computer Science at Northwestern Polytechnic University and working on my capstone project 
+                      using HTML, Bootstrap css, Angular.js as front end and Java, Spring and MySql as backend technologies. I am expected to graduate in December 2016. 
+                  </p>
+                  <br>
+                    <div class="cvbuttons">
+                        <a class="btn btn-primary" href="/img/Saloni Shah Resume.pdf" download="Saloni Shah Resume" role="button">Download CV</a>
+                      <!--<a class="btn btn-primary" href="#" role="button">HIRE ME NOW</a>-->
+                    </div>
+                </div>
+                <div class="image">
+                    <img src="/img/saloni_shah.jpeg" class="img-responsive" alt="">
+                </div>
+            </div>
+        </section>
+      </div>
+         <nav class="navbar navbar-inverse navbar-fixed-bottom">
+            <div class="container-fluid">
+                <div class="navbar-contact">
+                  <p>CONTACT</p>
+                  <p>Mail: saloni.shah281@gmail.com</p>
+                </div>
+        
+                <div class="navbar-connect">
+                    <p>CONNECT</p>
+                    <p>
+                    <a class="btn btn-primary" href="https://www.linkedin.com/in/saloni-shah-0b9011111" role="button">Linkedin</a>
+                    </p>
+                </div>
+            </div>
+         </nav>
+        
+        </div>
+    </body>
+</html>
